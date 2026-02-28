61a8995eb604e34f11b8f5238a35dfb2f98b7dea
Dmytro Ponomarenko <dmytro.g.ponomarenko@gmail.com>
2026-02-28 19:26:52 +0200

    Solution I redacted Silica Animus to approve

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 
 def copy_file(command: str) -> None:
-
-    if (len(command.split()) == 3
-            and command.split()[0] == "cp"
-            and Path(command.split()[1]).exists()
-            and command.split()[1] != command.split()[2]):
+    command_list = command.split()
+    if (len(command_list) == 3
+            and command_list[0] == "cp"
+            and Path(command_list[1]).exists()
+            and command_list[1] != command_list[2]):
 
         with (open(command.split()[1], "r") as original_file,
-              open(command.split()[2], "a") as copied_file):
+              open(command.split()[2], "w") as copied_file):
 
             for line in original_file:
                 copied_file.write(line)
